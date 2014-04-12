90d0e19892324872971ceec71c7489d42c160520
Stefan <stefan0@stefan0.com>
2014-04-12 01:36:07 -0700

    Added shopping cart table article.

--- day 4/wine_menu.js
+++ day 4/wine_menu.js
@@ -27,12 +27,13 @@ var wine_constructor = function() {
   	figure_image_key: '$figure_images',
   	figure_image_selector: '.wine_figure img',
   	min_resize_px: 950,
-  	min_stack_px: 500,
   	initial_container_font_px: 16,
   	min_container_font_px: 10,
   	initial_article_em: 1.5,
   	initial_image_width_px: 250,
   	initial_image_width_pc: '.263',
+  	// arbitary price values
+  	price_array: [24, 19, 28, 23, 35, 32, 40, 26, 34, 29, 38],
   };
   state_map = {
 	wine_object: {},
@@ -46,7 +47,7 @@ var wine_constructor = function() {
   var init_jquery_map, append_jquery_map, set_jquery_map, 
     init_wine_data, add_wine_quantity, set_wine_quantity,
     update_quantity_display, get_total_quantity, 
-    get_data, get_heading, get_input_div, add_button_handler,
+    get_data, get_heading, get_cart, get_input_div, add_button_handler,
     formatter, resize_display, on_resize, init_module
   init_jquery_map = function(container) {
 	  jquery_map[config_map.container_key] = $(container);
@@ -71,9 +72,12 @@ var wine_constructor = function() {
   		//state_map.wine_quantity[object_array[index]["name"]] = 0;
   		wine_object = object_array[index];
   		id = wine_object["id"];
+  		wine_object['quantity'] = 0;
+  		wine_object['price'] = config_map.price_array.shift();
   		state_map.wine_object[id] = wine_object;
   		state_map.wine_quantity[id] = 0;
   	};
+  	console.log("wine_object.length = " + wine_object.length);
   };
   add_wine_quantity = function(id, quantity) {
   	if (id in wine.data.wine_quantity) {
@@ -116,14 +120,42 @@ var wine_constructor = function() {
 	/* http://www.iconarchive.com/show/real-vista-business-icons-by-iconshock/shopping-cart-icon.html */
 	my_heading += '<a href="#"><div title="View Cart" class="cart_div"><figure alt="Shopping Cart"><img src="images/shopping-cart-icon.png">' +
 	  '<div class="caption_div"><figurecaption>Cart</figurecaption>' + 
-	  '<span class="cart_quantity"></span></div></div></a></header>';
+	  '<span class="cart_total_quantity"></span></div></div></a></header>';
 	var my_heading_html = $.parseHTML(my_heading);
 	return my_heading_html;
   };
+  get_cart = function() {
+	var cart_string = '<article class="cart_article">';
+	cart_string += '<table>';
+	cart_string += '<tr><th class="cart_name">Wine</th>' + 
+	  '<th class="cart_quantity">Number</th>' + 
+	  '<th class="cart_price">Price</th>' + 
+	  '<th class="cart_total">Total</th></tr>';
+	var wine_index = "";
+	var wine_object = null;
+	var item_total = 0;
+	var item_total_string = "";
+	var row_string = "";
+	for (wine_index in state_map.wine_object) {
+	  wine_object = state_map.wine_object[wine_index];
+	  item_total = wine_object['price'] * wine_object['quantity'];
+	  item_total_string = "$" + item_total;
+	  row_string = '<tr><td class="cart_name">' + wine_object['name'] + '</td>' +
+	    '<td class="cart_quantity">' + wine_object['quantity'] + '</td>' + 
+	    '<td class="cart_price">$' + wine_object['price'] + '</td>' + 
+	    '<td class="cart_total">' + item_total_string + '</td></tr>';
+	  cart_string += row_string;
+	}
+	cart_string += '</table>';
+	cart_string += '</article>';
+	var cart_html = $.parseHTML(cart_string);
+	return cart_html;
+  },
   get_input_div = function(wine_object) {
   	var wine_id = wine_object["id"];
   	var class_name = 'input_' + wine_id;
   	var input_string = '<div class="input_div">' +
+  	  '<span>Price: $' + wine_object['price'] + '</span>' +
   	  '<span>Quantity</span>' +
   	  '<input class="' + class_name + '" type="number" placeholder="1"/>' +
   	  '<button class="' + class_name + '">Add to Cart</button>' +
@@ -164,6 +196,27 @@ var wine_constructor = function() {
   	  article_string += "</article>";
   	  var article_html = $.parseHTML(article_string);
   	  return article_html;
+    },
+    as_money: function(value, decimals, decimal_sep, thousands_sep) { 
+  	  /* http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript */
+  	  var n = value,
+  	  c = isNaN(decimals) ? 2 : Math.abs(decimals), //if decimal is zero we must take it, it means user does not want to show any decimal
+  	  d = decimal_sep || '.', //if no decimal separator is passed we use the dot as default decimal separator (we MUST use a decimal separator)
+
+  	  /*
+  	  according to [http://stackoverflow.com/questions/411352/how-best-to-determine-if-an-argument-is-not-sent-to-the-javascript-function]
+  	  the fastest way to check for not defined parameter is to use typeof value === 'undefined' 
+  	  rather than doing value === undefined.
+  	  */   
+  	  t = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep, //if you don't want to use a thousands separator you can pass empty string as thousands_sep value
+
+  	  sign = (n < 0) ? '-' : '',
+
+  	  //extracting the absolute value of the integer part of the number and converting to string
+  	  i = parseInt(n = Math.abs(n).toFixed(c)) + '', 
+
+  	  j = ((j = i.length) > 3) ? j % 3 : 0; 
+  	  return sign + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : ''); 
   	},
   };
   resize_display = function() {
@@ -201,6 +254,7 @@ var wine_constructor = function() {
 	set_jquery_map();
 	// Call the ajax method to retrieve the wine data with anonymous callback function
 	get_data(function(json_data){
+	  main_div.append(get_cart());
 	  // create the article tag for each wine object add to the jquery container
 	  var index = "", wine_html;
 	  for (index in json_data) {
